dc10ac26dd776f7e7d77c6180f49261f804b79ab
TheMakingRooms <tom.macpherson-pope@makingrooms.org>
2022-11-24 12:57:06 +0000

    Update customclothes.py

--- customclothes.py
+++ customclothes.py
@@ -10,8 +10,9 @@ import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 lcd_columns = 16
 lcd_rows = 2
 
-clothes = ["Shirt","Skirt","Trousers","Shorts","Dress"]
-colours = ["Red","Green","Blue","Black","White"]
+options = [ ["Shirt","Skirt","Trousers","Shorts","Dress"],
+			["Red","Green","Blue","Black","White"]
+		  ]
 
 # Initialise I2C bus.
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -19,26 +20,21 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 # Initialise the LCD class
 lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
 
-lcd.clear()
-# Set LCD color to red
 
-lcd.cursor = True
+#lcd.cursor = True
 
-lcd.blink = True
+#lcd.blink = True
 
-#scroll_msg = "<-- Scroll"
-#lcd.message = scroll_msg
-# Scroll to the left
-#for i in range(len(scroll_msg)):
-#    time.sleep(0.5)
-#    lcd.move_left()
-#lcd.clear()
-#time.sleep(1)
-#lcd.message = "Going to sleep\nCya later!"
-#time.sleep(5)
+
+def refresh(i, j):
+	lcd.clear()
+	lcd.message=  '^v<> Options' +'\n' + option[i][j]
+	time.sleep(0.5)
+
 
 
 while True:
+	lcd.clear()
 	lcd.message= "Custom Clothing\nEnter to scan"
 	lcd.color = [0, 0, 100]
 
@@ -48,7 +44,7 @@ while True:
 	while lcd.select_button:
 		pass
 	lcd.clear()
-	lcd.message= "Scanning…\n Press Select to stop scanning"
+	lcd.message= "Scanning...\nSelect when done"
 	lcd.color = [100, 0, 0]
 	time.sleep(1)
 
@@ -57,21 +53,57 @@ while True:
 	time.sleep(0.5)		
 	while lcd.select_button:
 		pass
-		
+
 	lcd.clear()
-	lcd.message= "Scan successful\nLCD Press button for options"
+	lcd.message= "Scan successful\nSelect 4 options"
 	lcd.color = [0, 0, 100]
 
+	while not lcd.select_button:
+		pass
+	time.sleep(0.5)		
+	while lcd.select_button:
+		pass
 
 
-#if lcd.left_button:
-
-#if lcd.up_button:
-
-
-#On button release
+	lcd.clear()
+	lcd.message= "Scan successful\nSelect 4 options"
 
 
+	while not lcd.select_button:
+		pass
+	time.sleep(0.5)		
+	while lcd.select_button:
+		pass
 
-#<Cycle through options>
+	i = 0
+	j = 0
+	refresh(i,j)
+
+	while not lcd.select_button:
+		if lcd.up_button:
+			i = i+1% len(options)
+			j=0
+			refresh(i,j)
+
+		elif lcd.down_button:
+			i = i-1% len(options)
+			j=0
+			refresh(i,j)
+
+		elif lcd.left_button:
+			j = j-1% len(options[i])	
+			refresh(i,j)		
+
+		elif lcd.right_button:
+			j = j+1% len(options[i])
+			refresh(i,j)
+
+	time.sleep(0.5)		
+	while not lcd.select_button:
+		pass
+	lcd.clear()
+	lcd.message= "Options chosen\nOrder made"
+
+	
+
 
